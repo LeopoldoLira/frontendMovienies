e74d48890309e1c693aecfd719294dec89bf59a6
Leopoldo Lira <lira.leopoldo97@gmail.com>
2023-05-27 03:23:49 -0600

    fix(api)!: fixed typo of baseurl form axios instance

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-	baseURL: "http://liraleopoldo.pythonanywhere.com/",
+	baseURL: "https://liraleopoldo.pythonanywhere.com/",
 });
 
 API.interceptors.request.use((config) => {
